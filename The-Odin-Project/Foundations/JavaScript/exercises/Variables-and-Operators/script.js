83ab5f458ecfe1e75565d71cfa2809147d798b67
Omar-Fa <o.fathy.official@gmail.com>
2025-06-21 00:49:01 +0300

    Finished till JavaScript Developer Tools lesson

---
+++ The-Odin-Project/Foundations/JavaScript/exercises/Variables-and-Operators/script.js
@@ -0,0 +1,22 @@
+console.log(23 + 97);
+
+console.log(23 + 97 + 22 + 1 + 5 + 90);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 2;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(max);
+console.log(actual);
+console.log(percentage);
